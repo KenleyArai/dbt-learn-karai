a95bdbff078e16b3497e3507398c0662a4ba5044
Kenley Arai <karai@peerstreet.com>
2020-02-20 19:23:56 +0000

    Cleaned naming convention

--- models/raw/stg_stripe_payments.sql
+++ models/raw/stg_stripe_payments.sql
@@ -1,7 +1,7 @@
 select 
     id as stripe_payment_id,
     "orderID" as order_id,
-    "paymentMethod" as paymentmethod,
+    "paymentMethod" as payment_method,
     amount as amount,
     created as created
 from 
